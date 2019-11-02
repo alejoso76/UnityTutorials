4d1ce12312fb47b6c3f9a514963ec247ea9214eb
alejoso76 <alejandroosoriotrujillo@gmail.com>
2019-11-02 15:48:02 -0500

    Jump and camera controller added

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private PlayerController _player;
+    private Vector3 lastPosition;
+    private float distanceToMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToMove = _player.transform.position.x - lastPosition.x;
+        transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
+        lastPosition = _player.transform.position;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public  float SpeedForce;
+    public float jumpForce;
+    private Rigidbody2D _player;
+    public LayerMask groundLayer;
+    public bool ground;
+    public Transform ReferencePoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ground = Physics2D.OverlapCircle(ReferencePoint.position, 1, groundLayer);
+        _player.velocity = new Vector2 (SpeedForce, _player.velocity.y);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (ground)
+                _player.velocity = new Vector2  (_player.velocity.x, jumpForce);
+        }
+    }
+}
